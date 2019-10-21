14e91239c64b85481365264856b2450abd09655e
sigabatman <56817966+sigabatman@users.noreply.github.com>
2019-10-21 10:51:15 +0300

    Create garso paleidimas.php

---
+++ garso paleidimas.php
@@ -0,0 +1,12 @@
+#include <windows.h>
+#include <iostream>
+
+using namespace std;
+
+
+int main()
+{
+    Beep(523,500); // 523 hertz (C5) for 500 milliseconds
+    cin.get(); // wait
+    return 0;
+}
